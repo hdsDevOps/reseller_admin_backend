d84ae33e48d65960159da50d6b0332a431497c39
saunak99 <saunak99@gmail.com>
2024-11-14 14:48:37 +0530

    changes in voucher API service

--- voucherservices/middleware/auth.js
+++ voucherservices/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
+exports.verifyToken = async (req, res, next) => {
 
-authMiddleware = async (req, res, next) => {
   const token =
     req.headers.authorization?.split("Bearer ")[1] || req.headers.token;
 
@@ -10,17 +10,16 @@ authMiddleware = async (req, res, next) => {
       error: "No token provided",
       message: "Authentication token is required",
     });
+
   }
+
   try {
     const decodedToken = jwt.verify(token, process.env.CRYPTOTOKEN);
 
     req.user = decodedToken;
     next();
   } catch (error) {
-    res.status(401).json({ error: "Invalid token" });
+    res.status(401).json({status: 401, error: 'Invalid token' });
+
   }
 };
-
-module.exports = authMiddleware;
-
-

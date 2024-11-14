8efc2eb21316f7bf7ec89f214ebc147bead4fb79
saunak99 <saunak99@gmail.com>
2024-11-14 19:40:40 +0530

    changes in admin API service

--- adminservices/index.js
+++ adminservices/index.js
@@ -61,7 +61,7 @@ app.post("/adminservices/send-email", (req, res) => {
 app.use("/adminservices/forgot-password", forgotPasswordRoutes);
 app.use("/adminservices/customers", adminServicesRoutes);
 
-app.use(`/admin/api/v1`, adminRoutes);
+app.use('/admin/api/v1', adminRoutes);
 app.use('/subscription/api/v1', subscriptionRoutes);
 app.use('/notification/api/v1', notificationRoutes);
 app.use('/voucher/api/v1', voucherRoutes);

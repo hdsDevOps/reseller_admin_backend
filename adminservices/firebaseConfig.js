b5b6b41c6381bac4daf9a5f43e585ee8697bc03c
saunak99 <saunak99@gmail.com>
2024-11-15 18:26:13 +0530

    changes in admin API change

--- adminservices/firebaseConfig.js
+++ adminservices/firebaseConfig.js
@@ -1,7 +1,7 @@
 // firebaseConfig.js
 const admin = require("firebase-admin");
 //const serviceAccount = require("./dev-hds-gworkspace-firebase-adminsdk-6ke7i-aab97fb726.json");
-    
+      
 let key_details = process.env.FIRESTORE_DETAILS;
 
 let key = JSON.parse(Buffer.from(key_details, 'base64').toString('utf-8'));

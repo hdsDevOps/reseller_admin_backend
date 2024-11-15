815190141a1f251ff87e63c5c0a5a93c2ac87302
saunak99 <saunak99@gmail.com>
2024-11-15 13:26:47 +0530

    changes in admin API service

--- adminservices/routes/customer_route.js
+++ adminservices/routes/customer_route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const adminServicesRouter = express.Router();
 const router = express.Router();
 const path = require('path');
-const customerservice = require("../services/customer_service.js");
+const customerservice = require('../services/customer_service.js');
 const authmiddleware = require('../middleware/auth.js');
 const customercontroller = require('../controllers/customer_controller.js');
     

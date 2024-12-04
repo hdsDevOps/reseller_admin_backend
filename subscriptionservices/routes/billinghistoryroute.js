b242c038263a1dc53141c3cc89624ab966d9bd9a
saunak99 <saunak99@gmail.com>
2024-12-04 10:48:06 +0530

    changes in subscription file

--- subscriptionservices/routes/billinghistoryroute.js
+++ subscriptionservices/routes/billinghistoryroute.js
@@ -4,7 +4,7 @@ const billingservice = require('../services/billingservice.js');
 const { verifyToken } = require('../middleware/auth.js');
       
 // Get all the voucher List excluding Deleted records
-router.post('/billinghistory', async (req, res) => {
+router.post('/billinghistory',verifyToken, async (req, res) => {
     res.status(200).send(await billingservice.getrecordlist(req.body));
 })
   

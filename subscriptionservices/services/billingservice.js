8d15d8e8df3f8fa4418524b0b6e5f3284798bb70
saunak99 <saunak99@gmail.com>
2024-12-06 15:18:36 +0530

    lastest code from server

--- subscriptionservices/services/billingservice.js
+++ subscriptionservices/services/billingservice.js
@@ -8,39 +8,69 @@ async function getrecordlist(data) {
         domain: data.domain,
         start_date: data.start_date,
         end_date: data.end_date,
+        searchKey_start: data.search_data.toLowerCase(),
+        searchKey_end: data.search_data.toLowerCase() + '\uf8ff' 
       };
-    
-      const searchKey = data.search_data;
-  
+  let searchKeyStart = "";
+  let searchKeyEnd = "";
+  let endDate = "";
+  let startDate = "";
+  let domain = "";
       // Queries for partial matches on firstname, lastname, and email
       let query = db.collection('billing_history');
 
       if (filter.domain) {
-        const domain = new Date(filter.domain);
-        query = query.where("domain", "==", domain);
+        domain = filter.domain;
+        
       }
 
       if (filter.start_date) {
-        const startDate = new Date(filter.start_date);
-        query = query.where("start_date", ">=", startDate);
+        startDate = new Date(filter.start_date);
+        
       }
 
       if (filter.end_date) {
-        const endDate = new Date(filter.end_date);
-        query = query.where("end_date", "<=", endDate);
+        endDate = new Date(filter.end_date);
+        
+      }
+
+      if (filter.searchKey_start && filter.searchKey_end) {
+        searchKeyStart = filter.searchKey_start;
+        searchKeyEnd = filter.searchKey_end;
+
       }
       
-
-// Execute the query
-const transactionQuery = await query.get();
+      
 
 
-      const customers = [];
-      transactionQuery.forEach((doc) => {
-        customers.push({
-          ...doc.data(),
-        });
-      });
+      const query1 = db
+  .collection("billing_history")
+  .where("customer_name", ">=", searchKeyStart)
+  .where("domain", "==", domain)
+  .where("created_at", ">=", startDate)
+  .where("created_at", "<=", endDate)
+  .where("customer_name", "<=", searchKeyEnd);
+
+const query2 = db
+  .collection("billing_history")
+  .where("transaction_id", ">=", searchKeyStart)
+  .where("domain", "==", domain)
+  .where("created_at", ">=", startDate)
+  .where("created_at", "<=", endDate)
+  .where("transaction_id", "<=", searchKeyEnd);
+
+const [result1, result2] = await Promise.all([query1.get(), query2.get()]);
+
+// Merge results
+const customers = [
+  ...result1.docs.map((doc) => ({ id: doc.id, ...doc.data() })),
+  ...result2.docs.map((doc) => ({ id: doc.id, ...doc.data() })),
+];
+
+// Remove duplicates
+const uniqueCustomers = customers.filter(
+  (value, index, self) => index === self.findIndex((t) => t.id === value.id)
+);
       return {
         status: 200,
         data: customers,
